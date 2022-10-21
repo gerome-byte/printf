2334e448e606894aa9b0973926dcea83d9c2a691
gerome-byte <geromeaidan@gail.com>
2022-10-20 23:34:54 -0700

    _printf.c

---
+++ _printf.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _printf - prints to the output
+ * @format: pointer
+ * Return: input len
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list arg;
+	int done;
+
+	va_start(arg, format);
+	done = vfprintf(stdout, format, arg);
+	va_end(arg);
+
+	return (done);
+}
